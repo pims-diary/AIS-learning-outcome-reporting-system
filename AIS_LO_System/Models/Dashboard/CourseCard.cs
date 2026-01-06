2775e1221afc63a39d12d73ba4504f8c9fb12164
Priyanka Ghosh <speak2priyankaghosh@gmail.com>
2026-01-06 22:22:54 +0530

    Creating Lecturer's dashboard in Model

---
+++ AIS_LO_System/Models/Dashboard/CourseCard.cs
@@ -0,0 +1,9 @@
+﻿namespace LOARS.Web.Models.Dashboard
+{
+    public class CourseCard
+    {
+        public string Code { get; set; } = "";
+        public string Title { get; set; } = "";
+        public string School { get; set; } = "Information Technology";
+    }
+}
---
+++ AIS_LO_System/Models/Dashboard/LecturerDashboardViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace LOARS.Web.Models.Dashboard
+{
+    public class LecturerDashboardViewModel
+    {
+        public int SelectedYear { get; set; }
+        public int SelectedTrimester { get; set; }
+
+        // For hamburger menu
+        public List<int> Years { get; set; } = new();
+        public Dictionary<int, List<int>> TrimestersByYear { get; set; } = new();
+
+        // Courses for selected year/trimester
+        public List<CourseCard> Courses { get; set; } = new();
+
+        public string ContextLabel => $"{SelectedYear} - Trimester {SelectedTrimester}";
+    }
+}
