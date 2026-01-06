435a3341255b213bdf525107d1aa18998da5110c
Priyanka Ghosh <speak2priyankaghosh@gmail.com>
2026-01-06 22:34:59 +0530

    Updated courses + Years/Trimesters view

---
+++ AIS_LO_System/Controllers/CourseDashboardController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LOARS.Web.Controllers
+{
+    [Authorize(Roles = "Lecturer")]
+    public class CourseDashboardController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(string courseCode, int year, int trimester)
+        {
+            ViewBag.CourseCode = courseCode;
+            ViewBag.Year = year;
+            ViewBag.Trimester = trimester;
+            return View();
+        }
+    }
+}
--- AIS_LO_System/obj/Debug/net8.0/AIS_LO_System.AssemblyInfo.cs
+++ AIS_LO_System/obj/Debug/net8.0/AIS_LO_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AIS_LO_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2775e1221afc63a39d12d73ba4504f8c9fb12164")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7acbf06778309d0acaa2429f26dbf93051e89c27")]
 [assembly: System.Reflection.AssemblyProductAttribute("AIS_LO_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AIS_LO_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
