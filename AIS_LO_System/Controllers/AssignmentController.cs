197274784997794569ebd698dcc07e87b1d3e53b
ronbrngn020203 <ronnielbarangan@gmail.com>
2026-01-25 16:33:41 +0300

    added the assignment dashboard

---
+++ AIS_LO_System/Controllers/AssignmentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AIS_LO_System.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(int id, string courseCode, string courseTitle)
+        {
+            ViewBag.AssignmentId = id;
+            ViewBag.CourseCode = courseCode;
+            ViewBag.CourseTitle = courseTitle;
+            return View();
+        }
+    }
+}
