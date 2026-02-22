867dfd07301dac01ccabc89f8667197484a3d63b
Andrew3605 <207550351+Andrew3605@users.noreply.github.com>
2026-02-22 21:57:48 +1300

    Epic 8: Improvements and bug fixes

--- AIS_LO_System/obj/Debug/net8.0/AIS_LO_System.AssemblyInfo.cs
+++ AIS_LO_System/obj/Debug/net8.0/AIS_LO_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AIS_LO_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f807ea798a0304030ea5c657b2facabd3c8cd992")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6fc03cfb74c75d493dc37e736a8cd4818d8899c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AIS_LO_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AIS_LO_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
