c25551465b1929b086551dfec076df7b6da38512
ronbrngn020203 <ronnielbarangan@gmail.com>
2026-04-02 00:37:12 +1300

    added missing codes in AddIsActiveUsers migration

--- AIS_LO_System/Migrations/20260329213537_AddIsActiveToUsers.cs
+++ AIS_LO_System/Migrations/20260329213537_AddIsActiveToUsers.cs
@@ -10,13 +10,20 @@ namespace AIS_LO_System.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "AppUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: true);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "AppUsers");
         }
     }
 }
